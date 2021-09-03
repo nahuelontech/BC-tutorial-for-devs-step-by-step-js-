15e2e0ec16ce996113001b55d02443b6d122df18
nahuelontech <87518314+nahuelontech@users.noreply.github.com>
2021-09-03 18:30:01 +0200

    Update 1_initial_migration.js

--- migrations/1_initial_migration.js
+++ migrations/1_initial_migration.js
@@ -1,3 +1,6 @@
+//If u´ve ever used another development framework, like for build mobile app or any kinda app that uses a database
+//seguramente hayas visto un migration file. A migrations means that u are moving something from one state to another 
+//In this case we are moving our SC to the blockchain, also means that we are updating the blockchain state to another
 const Migrations = artifacts.require("Migrations");
 
 module.exports = function(deployer) {
