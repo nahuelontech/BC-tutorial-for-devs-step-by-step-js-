5e1b5bd4d543324113f7062229dd96894d38070d
nahuelontech <87518314+nahuelontech@users.noreply.github.com>
2021-09-04 09:18:30 +0200

    Update SocialNetwork.sol

--- src/contracts/SocialNetwork.sol
+++ src/contracts/SocialNetwork.sol
@@ -1,17 +1,22 @@
 pragma solidity ^0.5.0;
 //1. THIS IS THE FIRST SC THAT WE´LL RUN
+//3. WE WANT OUR CONTRACT to create posts, lists all the posts, and tip posts
+//GO TO SOCIALNETWORK.JS FOR 3.1.
 contract SocialNetwork {
     string public name;  //1.1  //From here he goes to migrations -> initial and deploy 1.2
+//3.2.1. We don´t wanna hardcode the  postcreated(sin lo de abajo se quedaba un código feo)
+//This will keep track of the number of posts that gets added to the mapping below.
     uint public postCount = 0;
-    mapping(uint => Post) public posts;
-
+//3.2 The uint is going to be the id of the post and the value is going to be the post struct
+    mapping(uint => Post) public posts; 
+//3.2. Once we have this we hve to create a new post inside of the function. And also we need the mapping.
     struct Post {
         uint id;
         string content;
         uint tipAmount;
         address payable author;
     }
-
+//3.2.5 .We are creating this event to track the values that were stored inside of the post
     event PostCreated(
         uint id,
         string content,
@@ -29,30 +34,39 @@ contract SocialNetwork {
     constructor() public { //1.1
         name = "Dapp University Social Network";
     }
-
+//3.2. We gotta create a function that allows users to create posts
     function createPost(string memory _content) public {
-        // Require valid content
+//3.2.7.this just takes the content which is a string and converts it into a bytes array. 
+//Si fuese falso no continuaría con lo q tiene debajo
         require(bytes(_content).length > 0);
-        // Increment the post count
+        // Increment the post count, if u see up we can see that we are incrementing the value 3.2.1
         postCount ++;
-        // Create the post
+        // Create the post. As u see after created the mapping and the struct we can put posts =
+        //and use that value with the array 3.2.1. This creates the post and add it to the mapping
+        //GO TO 3.2.2 IF IN .JS
         posts[postCount] = Post(postCount, _content, 0, msg.sender);
-        // Trigger event
+//3.2.5.We are creating this event to track the values that were stored inside of the post. run the test with truffle test
+//Para 3.2.6 ve a test
         emit PostCreated(postCount, _content, 0, msg.sender);
     }
-
+//3.3.2.
     function tipPost(uint _id) public payable {
-        // Make sure the id is valid
+        // Make sure the id is valid 
         require(_id > 0 && _id <= postCount);
-        // Fetch the post
+        // Fetch the post 3.3.2. FIRST WE R GONNA FETCH THE POST FROM THE BC, SO WE CAN FETCH IT OUT OF THE MAPPING FROM UPSIDE
+        // THIS WILL CREATE A NEW COPY OF IT AND WE WILL UPDATE THESE VALUES(POST _POST..) AND THAT WON´T AFFECT THE POST ON THE BC
+        //IT WON´T AFFECT IT UNTIL WE REASSIGN POSTS[_ID]  GO TO 3.3.3 NO
         Post memory _post = posts[_id];
         // Fetch the author
         address payable _author = _post.author;
         // Pay the author by sending them Ether
         address(_author).transfer(msg.value);
-        // Incremet the tip amount
+        // Incremet the tip amount 3.3.4. (WE NEED TO ADD THE ACTUAL TIP THATS COMMING IN. BUT HOW DO WE DO THAT?
+        //ES DIFICIL XQ LO Q TENEMOS ES UN ARG en function(uint _id), solo tenemos el ID del post q queremos pass in. So wheres the actuall tip?
+        //wheres the crypto itself?we´re tipping this post with ether but how do u tell this function that u want to tip ETHER, theres no argument for the
+        //amount. Solidity nos deja usar mas function metadata just like msg.sender de arriba to track the amount of ether they get sent in whenerver this functions is called
         _post.tipAmount = _post.tipAmount + msg.value;
-        // Update the post
+        // Update the post 3.3.3. WE´LL UPDATE THE POST NOW BEFORE I MANIPULATE IT. SE UPDATEA DESPUES DE HAVER ECHO LO DE AUTOR,TIP AMOUNT...
         posts[_id] = _post;
         // Trigger an event
         emit PostTipped(postCount, _post.content, _post.tipAmount, _author);
